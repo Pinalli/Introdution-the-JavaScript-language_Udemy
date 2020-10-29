9e07a6807698d64e815ccb628569e969f331e835
Alberto Rocha Pinalli <betopinalli@gmail.com>
2020-10-29 19:01:02 -0300

    created project javaScript

---
+++ Js_Project/script.js
@@ -0,0 +1,71 @@
+//function mensagem(nome){
+//alert ("Hei, não clique em mim, "+nome );
+//}
+
+function mudaCor(cor){
+    var elemento = document.getElementById("mensagem");
+    elemento.style.color = cor;
+}
+
+
+
+function soma(a,b){
+    console.log(a+b);
+}
+soma(47,10);
+
+
+function sub(a,b){
+   return a - b;
+}
+var s = sub(78,3)
+console.log(s);
+
+
+
+function mult(a,b){
+    return a * b;
+ }
+console.log(mult(3,4));
+
+
+
+/*var mensagem = "Hello World!" ;
+alert(mensagem);
+var a = 0;
+var b = 3;
+var c = "Hello";
+
+var lista = ["Cuba", "França", "Brasil", c]
+    alert(lista);
+    alert(lista[1]);
+
+for(i in lista){
+    alert(lista[i]);
+}
+*/
+/*while(a < 3){
+    alert(a);
+    a++;
+} 
+
+for(var i = 0; i < 4; i++){
+    alert(i);
+} */
+
+
+/*var soma = a + b;
+var sub = a - b;
+var mult = a * b;
+var div = a / b;
+
+/*alert(a%b);
+if (c%2 == 1){
+ alert("Numero impar")
+}
+else if(c%2 == 0){
+    alert("Numero par")
+}
+else{
+    alert("Valor inválido")
+}*/
